3600f5708150aabf405ea5fb9b42fa5c733672b1
Daniel Aquino-Cano <daquinocano20021@columbushs.com>
2019-12-10 10:27:46 -0500

    Done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -49,4 +49,56 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack(){
+        let aViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+        let aSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+        aSaxon.receiveDamage(aViking.strength);
+
+        if(aSaxon.health <= 0){
+            this.saxonArmy.splice(this.saxonArmy.indexOf(aSaxon));
+            return "A Saxon has died in combat";
+        }
+        else{
+            return ("A Saxon has received " + aViking.strength + " points of damage");
+        }
+        
+    }
+    saxonAttack(){
+        let aViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+        let aSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+        aViking.receiveDamage(aSaxon.strength);
+
+        if(aViking.health <= 0){
+            this.vikingArmy.splice(this.vikingArmy.indexOf(aViking));
+            return (aViking.name + " has died in act of combat");
+            
+        }
+        else{
+            return (aViking.name + " has received " + aSaxon.strength + " points of damage");
+        }
+        
+        
+    }
+    showStatus(){
+        if(this.saxonArmy.length == 0){
+            return "Vikings have won the war of the century!";
+        }
+        else if(this.vikingArmy.length == 0){
+            return "Saxons have fought for their lives and survive another day...";
+        }
+        else{
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
+}
